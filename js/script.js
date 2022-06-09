f93e0f6469e9999d0e609bacf3790595ad628290
username <username@example.com>
2022-06-10 00:23:20 +0900

    作品リンク先追加

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ $(function(){
 	$('.slider').slick({
 		autoplay: true,//自動的に動き出すか。初期値はfalse。
 		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		speed: 500,//スライドのスピード。初期値は300。
+		speed: 300,//スライドのスピード。初期値は300。
 		slidesToShow: 3,//スライドを画面に3枚見せる
 		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
 		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
