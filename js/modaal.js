7b0ac0e6f6ddb5bc2ed50686f69e4a23bd8a5e2b
username <username@example.com>
2022-06-10 22:18:09 +0900

    動画、アニメ修正

--- js/modaal.js
+++ js/modaal.js
@@ -36,7 +36,7 @@ $(function(){
 	$(".video-open").modaal({
 		type: 'video',
 		overlay_close:true,//モーダル背景クリック時に閉じるか
-		background: '#28BFE7', // 背景色
+		background: '#525252', // 背景色
 		overlay_opacity:0.8, // 透過具合
 		before_open:function(){// モーダルが開く前に行う動作
 			$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
